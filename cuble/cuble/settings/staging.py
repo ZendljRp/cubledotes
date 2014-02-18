b9e328d508c1157f6666eecbbd9ee5123370e5b3
Marcos Gabarda <marcosgabarda@gmail.com>
2014-02-18 16:41:31 +0100

    storages

--- cuble/cuble/settings/staging.py
+++ cuble/cuble/settings/staging.py
@@ -61,5 +61,5 @@ DATABASES = {
 
 ########### STORAGES CONFIGURATION
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATIC_URL = "https://{}.s3.amazonaws.com/".format(AWS_STORAGE_BUCKET_NAME)
+STATIC_URL = "//{}.s3.amazonaws.com/".format(AWS_STORAGE_BUCKET_NAME)
 ########### END STORAGES CONFIGURATION
