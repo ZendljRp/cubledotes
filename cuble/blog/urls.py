1f0f814f9b9fff9ef76c69f5146c8fbc7a03270e
Marcos Gabarda <marcosgabarda@gmail.com>
2014-02-18 13:29:17 +0100

    favicons

--- cuble/blog/urls.py
+++ cuble/blog/urls.py
@@ -24,15 +24,22 @@ THE SOFTWARE.
 
 """
 from django.conf.urls import patterns, url
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
 from blog.views import PostsListView, PostDetailsView, PostsTagListView, PostsFeed, PostsAtomFeed
 
 
 urlpatterns = patterns(
     '',
     url(r'^$', PostsListView.as_view(), name="blog"),
+
     url(r'^feed/', PostsFeed(), name="blog_feed"),
     url(r'^atom/', PostsAtomFeed(), name="blog_atom"),
+    # Redirecciones
+    url(r'^rss/', RedirectView.as_view(url=reverse_lazy('blog_feed'))),
+
     url(r'^tag/(?P<slug>.+)/$', PostsTagListView.as_view(), name="posts_tag"),
     url(r'^(?P<slug>.+)/$', PostDetailsView.as_view(), name="post"),
+
 )
 
