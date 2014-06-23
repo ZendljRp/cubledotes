1270e7e1384b6e8357a9b22a16b25814c9c6587b
Marcos Gabarda <marcosgabarda@gmail.com>
2014-06-23 18:59:20 +0200

    settings

--- cuble/cuble/settings/production.py
+++ cuble/cuble/settings/production.py
@@ -35,6 +35,13 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 ########## END DEBUG CONFIGURATION
 
+########## HOST CONFIGURATION
+# See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
+ALLOWED_HOSTS = [
+    'cuble.pythonanywhere.com',
+    '.cuble.es',
+]
+########## END HOST CONFIGURATION
 
 ########## EMAIL CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
